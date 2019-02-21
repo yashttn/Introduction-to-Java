ba2e0d1ca2843f67a61510444fcea024abc0b775
Yashwant <yashwant.vadali@tothenew.com>
2019-02-21 11:36:01 +0530

    added ques9

--- Java1/Ques9.java
+++ Java1/Ques9.java
@@ -1,11 +1,32 @@
 package Java1;
 
-enum hello{
+enum Rent{
+    Delhi(1000),
+    Chennai(2000),
+    Bangalore(3000),
+    Hyderabad(4000);
+
+    private final int price;
+
+    Rent(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 
 }
-
 public class Ques9 {
+    Rent rent;
+    Ques9(Rent rent){
+        this.rent = rent;
+    }
+
     public static void main(String[] args) {
+        for(Rent house : Rent.values()){
+            System.out.println("House name : "+house+"\nRent : "+house.getPrice());
+        }
 
     }
 }
